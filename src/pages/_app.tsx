9f28ec026acb67614fb1752f320751ca057853a4
mlc4ca <mlc4ca@bosch.com>
2025-03-05 09:40:32 -0300

    SEO: meta description

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,21 @@
 import "@/styles/globals.css";
 import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Cotação de seguro gratuita</title>
+        <meta
+          name="description"
+          content="Proteja seu veículo com a KM Proteção Veicular! Atendimento e suporte via WhatsApp e planos sem burocracia. Segurança e tranquilidade para você rodar sem preocupações!"
+        />
+      </Head>
       <GoogleAnalytics gaId="G-PPDLLHZ6B7" />
-      <GoogleTagManager gtmId="GTM-KTZLT7TC" />
       <Component {...pageProps} />
+      <GoogleTagManager gtmId="GTM-KTZLT7TC" />
     </>
   );
 }
